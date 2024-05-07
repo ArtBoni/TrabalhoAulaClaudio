508857b8cdd086f18c85309296de49936fff0b4b
Arthur Bonelli <artbonelli@gmail.com>
2024-05-07 15:51:47 -0300

    Commit 7
    Codigos

--- AulaTrabalhoClaudio/Assets/Scripts/Player.cs
+++ AulaTrabalhoClaudio/Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform foot;
+    public Rigidbody2D body;
+    bool groundCheck;
+    public float horizontal;
+    public float speed = 5, jumpStrength = 5, bulletSpeed = 8;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject temp = Instantiate(bullet, transform.position, transform.rotation);
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+        }
+        horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+        groundCheck = Physics2D.OverlapCircle(foot.position, 0.05f);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+        if (Input.GetButtonDown("Jump") && groundCheck)
+        {
+            body.AddForce(new Vector2(0, jumpStrength * 100));
+        }
     }
 }
