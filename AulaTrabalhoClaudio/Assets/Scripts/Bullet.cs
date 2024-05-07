5e99a56578fe41559eda16ddaa090d6cc61af9fa
Arthur Bonelli <artbonelli@gmail.com>
2024-05-07 16:01:34 -0300

    Commit 8
    Finalizando os códigos

---
+++ AulaTrabalhoClaudio/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int life;
+    public int damage;
+    public BoxCollider2D boxCollider;
+    public Rigidbody2D body;
+    public GameObject player;
+    public float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- AulaTrabalhoClaudio/Assets/Scripts/Enemy.cs
+++ AulaTrabalhoClaudio/Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int life;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,17 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            life -= collision.gameObject.GetComponent<Bullet>().damage;
+            Destroy(collision.gameObject);
+            if (life <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
